b7d28cccee1ee5982e034a1a2ed4480ae4d90b58
tranb3r <tranb3r@gmail.com>
2024-08-01 14:29:16 +0200

    move app code to folder

---
+++ DotNetMaui/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DotNetMaui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			// This adds Sentry to your Maui application
+            .UseSentry(options =>
+            {
+                // The DSN is the only required option.
+                options.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+                options.Debug = true;
+                // Attach screenshots on errors
+                options.AttachScreenshot = true;
+            })
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
