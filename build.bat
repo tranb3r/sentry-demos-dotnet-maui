bca37738ef815e4e62ecdb6802e13349e1ebe862
tranb3r <tranb3r@gmail.com>
2024-08-02 16:12:30 +0200

    add scripts

---
+++ build.bat
@@ -0,0 +1,1 @@
+dotnet build -bl -v m DotNetMaui\DotNetMaui.csproj -c Release -f net8.0-android -r android-x64
---
+++ install.bat
@@ -0,0 +1,1 @@
+adb install -r DotNetMaui\bin\Release\net8.0-android\android-x64\com.companyname.dotnetmaui-Signed.apk
